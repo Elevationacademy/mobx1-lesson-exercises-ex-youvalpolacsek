d5eb30ba92414e07706afd8b6d8db5a93f561b2e
Youval Polacsek <Youval@h-MacBook-Pro-sl-Youval.local>
2019-08-19 13:02:15 +0300

    really done

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,18 +6,14 @@ class Item extends Component {
     checkItem = (e) => {
       this.props.store.checkItem(e.target.value)
     }
-      editItem = (location, name) => {
-        this.props.store.editItem(location, name)
+      editItem = () => {
+        let location = prompt("edit location", "new location")
+        this.props.store.editItem(location, this.props.item.name)
       }
       deleteItem = () => {
         this.props.store.deleteItem(this.props.item.name)
       }
-      getPromped = () => {
-        let location = prompt("edit location", "new location")
-        this.editItem(location, this.props.item.name)
-      }
-
-      
+   
     render() {
       let store = this.props.store
       let item = this.props.item
@@ -26,14 +22,9 @@ class Item extends Component {
               <div className = {item.completed ? "crossed" : null}>
               <input type="checkbox" onClick={this.checkItem} value={item.name}/>
               {item.name} {item.location}
-              <button className="editButton" onClick={this.getPromped}>edit location</button>
+              <button className="editButton" onClick={this.editItem}>edit location</button>
               <button className="delete" onClick={this.deleteItem}>delete</button>
               </div>
-      {/*   your code here
-            each item should be in an input checkbox
-            it should display the item name and location
-            it should have both an edit button and a delete button
-      */} 
             </div>)
     }
 }
